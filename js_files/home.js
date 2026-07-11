c0c22c19beb125e75132bf8aec1819863b5558b0
Filemon Sang <filemonsang@gmail.com>
2026-07-11 08:32:58 -0400

    -update comment rendering logic, comment functionality started

--- js_files/home.js
+++ js_files/home.js
@@ -28,3 +28,48 @@ commentButtonClose.forEach((button)=>{
         commentModal.classList.remove('active');
     })
 })
+
+/*USER COMMENTING FUNCTION*/
+
+const commentInput = document.querySelectorAll('.comment-input');
+const comments = []
+const commentSendButton = document.querySelectorAll('.comment-send-button')
+const commentArea = document.querySelectorAll('.comment-modal-body')
+
+
+
+const renderComment = ()=>{
+    let html = ''
+    comments.forEach((comment)=>{
+        html += `
+        <div class="comment user-comment">
+                
+            <div class="comment-content">
+                <h style = "font-weight: 'bold'">You</h>
+                <p class="comment-text">${comment.userComment}</p>
+            </div>
+        </div>
+        `
+    })
+    commentArea.forEach((comment)=>{
+        comment.innerHTML = html
+    })
+    console.log(html);
+}
+
+const sendComment = () =>{commentSendButton.forEach((comment)=>{
+    comment.addEventListener('click',(event)=>{
+        const parentPost = event.target.closest('.comment-modal-footer');
+        const specificInput = parentPost.querySelector('.comment-input');
+        const userComment = specificInput.value
+        comments.push(
+            {
+                userComment:  userComment
+            }
+        )
+        renderComment();
+    })
+})}
+
+sendComment();
+
