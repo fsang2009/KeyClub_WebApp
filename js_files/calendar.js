2ee6487c92dff112cfcee839db2833029514687b
Filemon Sang <filemonsang@gmail.com>
2026-07-13 08:38:55 -0400

    update- calendar logic 70% finished, making safe checkpoint incase of future detrimental bugs.

--- js_files/calendar.js
+++ js_files/calendar.js
@@ -19,3 +19,66 @@ starts at index 0, so january is 0 , december is 11)
 
 Step 5. idfk just start on it
 */
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+
+const calendarGrid = document.querySelector('.calendar-grid');
+//Get all days in a month
+
+const currentDateInfo = new Date()
+// Render Calendar function 
+const renderCalendar =(month, year)=>{
+const firstDayofMonth = new Date(year, month, 1).getDay();
+const daysInMonth = new Date(year, month + 1, 0).getDate();
+const daysInPrevMonth = new Date(year, month, 0).getDate();
+console.log(firstDayofMonth)
+let dateCell = ''
+const fillInCellDates = daysInPrevMonth - firstDayofMonth + 1
+
+for(d=fillInCellDates; d<=daysInPrevMonth; d++){
+    dateCell +=`
+    <div class="calendar-day outside-month">
+                        <span class="day-number">${d}</span>
+                    </div>
+    `
+}
+    for(i=1; i <= daysInMonth; i++){
+        
+        dateCell +=`<div class="calendar-day">
+                        <span class="day-number">${i}</span>
+                    </div>`
+    }
+
+    calendarGrid.innerHTML = dateCell
+}
+
+
+
+const currentMonth = currentDateInfo.getMonth();
+const currentYear = currentDateInfo.getFullYear();
+renderCalendar(currentMonth, currentYear);
+
+
+// Change as we move forward in months
+
+const previousMonthButton = document.querySelector('#previousMonth');
+const forwardMonthButton = document.querySelector('#nextMonth');
+
+previousMonthButton.addEventListener('click',()=>{
+
+
+})
+    
